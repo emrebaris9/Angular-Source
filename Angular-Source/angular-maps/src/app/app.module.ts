7119517c561b5a74d2f6bb3ce4028d2e6431cec0
emrebaris9 <emrbaris@gmail.com>
2019-03-15 10:04:02 +0300

    first commit

---
+++ Angular-Source/angular-maps/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AgmCoreModule } from '@agm/core';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'YOUR_KEY'
+    })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
